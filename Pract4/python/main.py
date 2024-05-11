4c8c52e0d261dfa2778c210a1f09e70280cd4ee3
broGideon <turuncev70@gmail.com>
2024-05-12 00:56:03 +0300

    Pract4 90% complite

--- Pract4/python/main.py
+++ Pract4/python/main.py
@@ -1,14 +1,52 @@
 import contract_info 
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
+import string
 
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 contract = w3.eth.contract(address=contract_info.contract_address, abi=contract_info.abi)
 
+def input_int(info):
+    try:
+        data = int(input(info))
+        return data
+    except:
+        print("Неверный тип данных")
+        return False
+    
+def secure(password):
+    if password.count < 12:
+        print("Пароль должен быть больше 12 символов")
+        return False
+
+    if not any(item.islower() for item in password):
+        print("Пароль должен содержать прописные буквы")
+        return False
+    
+    if not any(item.isupper() for item in password):
+        print("Пароль должен содержать строчные буквы")
+        return False
+    
+    if not any(item.isdigit() for item in password):
+        print("Пароль должен содержать числа")
+        return False
+    
+    if not any(item in string.punctuation for item in password):
+        print("Пароль должен содержать спец символы")
+        return False
+
+    return True
+
 def register():
-    password = input("Введите пароль: ")
+    is_secure = False
+    while(not is_secure):
+        password = input("Введите пароль: ")
+
+        is_secure = secure(password)
+
+
     account = w3.geth.personal.new_account(password)
     print(f"Ваш публичный ключ: {account}")
 
@@ -20,7 +58,7 @@ def login():
         print("Авторизация прошла успешно!")
         return public_key
     except Exception as e:
-        print("Ошибка авторизации: {e}")
+        print(f"Ошибка авторизации: {e}")
         return None
 
 def create_estate(account):
@@ -39,24 +77,54 @@ def create_estate(account):
             'from': account,
         })
 
-        print(f"Ваша транзакция успешно отправлена. Хэш транзакции: {tx_hash.hex()}")
+        print(f"Операция успешно выполнена. Хэш операции: {tx_hash}")
     except Exception as e:
-        print(f"Ошибка: {e}")
+        print(f"Ошибка создания объявления: {e}")
 
 def create_ad(account):
     try:
         prise = int(input("Введите цену: "))
         get_estate(account)
         id_estate = int(input("Введите ID: "))
-        is_active = int(input("1. Open \n2. Close\nВыберите активность объявления: "))
+        ad_status = int(input("1. Open \n2. Close\nВыберите активность объявления: "))
 
-        tx_hash = contract.functions.createAd(prise, id_estate, is_active).transcat({
+        if ad_status < 1 or ad_status > 2:
+            print("Недопустимое значение")
+            return
+
+        tx_hash = contract.functions.createAd(prise, id_estate, ad_status-1).transact({
             'from': account,
         })
 
-        print(f"Ваша транзакция успешно отправлена. Хэш транзакции: {tx_hash.hex()}")
+        print(f"Операция успешно выполнена. Хэш операции: {tx_hash}")
     except Exception as e:
         print(f"Ошибка создания объявления: {e}")
+
+def change_status_estate(account):
+    try:
+        get_estate(account)
+        id_estate = int(input("Введите ID: "))
+
+        tx_hash = contract.functions.changeStatusEstate(id_estate).transact({
+            'from': account 
+        })
+
+        print(f"Операция успешно выполнена. Хэш операции: {tx_hash}")
+    except Exception as e:
+        print(f"Ошибка смены статуса: {e}")
+
+def change_status_ad(account):
+    try:
+        get_ads(account)
+        id_ad = int(input("Введите ID: "))
+
+        tx_hash = contract.functions.changeStatusAd(id_ad).transact({
+            'from': account 
+        })
+
+        print(f"Операция успешно выполнена. Хэш операции: {tx_hash}")
+    except Exception as e:
+        print(f"Ошибка смены статуса: {e}")
 
 def get_balance(account):
     try:
@@ -66,6 +134,30 @@ def get_balance(account):
         print(f"Ваш баланс на смарт-контракте: {balance}")
     except Exception as e:
         print("Ошибка получения баланса: {e}")
+
+def buy_estate(account):
+    try:
+        get_ads(account)
+        id_ad = int(input("Введите ID: "))
+        to = input("Введите адрес:")
+        tx_hash = contract.functions.buyEstate(id_ad).transact({
+            'from' : account
+        })
+
+        print(f"Операция успешно выполнена. Хэш операции: {tx_hash}")
+    except Exception as e:
+        print(f"Ошибка транзакции: {e}")
+
+def withdraw(account):
+    try:
+        value = int(input("Введите кол-во WEI для вывода: "))
+        tx_hash = contract.functions.withDraw(value).transact({
+            'from' : account
+        })
+
+        print(f"Операция успешно выполнена. Хэш операции: {tx_hash}")
+    except Exception as e:
+        print(f"Ошибка транзакции: {e}")
 
 def get_estate(account):
     try:
@@ -87,25 +179,29 @@ def get_ads(account):
         for item in ads:
             print(item)
     except Exception as e:
-        print("Ошибка получения баланса: {e}")
+        print("Ошибка получения объявлений: {e}")
 
-def withdraw(account):
+def pay(account):
     try:
-        value = int(input("Введите кол-во WEI для отправки: "))
-        to = input("Введите адрес:")
-        tx_hash = contract.functions.withdrawll(to, value).transact({
-            'from' : account
+        value = int(input("Введите кол-во WEI для пополнения: "))
+        if value <= 0:
+            print("Значение должно быть больше нуля") 
+            return
+        tx_hash = contract.functions.pay().transact({
+            'from' : account,
+            'value': value
         })
-        print(f"ваша транзакция успешно отправлена. Хэш транзакции: {tx_hash.hex()}")
+
+        print(f"Операция успешно выполнена. Хэш операции: {tx_hash}")
     except Exception as e:
-        print(f"Ошибка отправки WEI: {e}")
+        print(f"Ошибка пополнения: {e}")
 
 
 def main():
     account = "";
     while True:
         if account == "" or account == None:
-            choice = int(input("Долбро пожаловать в систему! Выберите: 1. Регистрация 2. Авторизация 3. Выйти"))
+            choice = int(input("Долбро пожаловать в систему! Выберите: 1. Регистрация 2. Авторизация 3. Выйти \n"))
             match choice:
                 case 1:
                     register()
@@ -116,20 +212,20 @@ def main():
                 case _:
                     print("Выберите 1 или 2!")
         else:
-            choice = int(input("Выберите: 1. Создать недвижимость \n2. Создать объявление \n3. Сменить статус недвижимости \n4.  Сменить статус объявления \n5. Покупка недвжимости \n6. Вывод средств \n7. Доступная недвижимость \n8. Доступные объявления \n9. Баланс на смарт контракте \n10. Баланс на аккаунте \n11. Пополнить баланс \n12. Выход"))
+            choice = int(input("Выберите: \n1. Создать недвижимость \n2. Создать объявление \n3. Сменить статус недвижимости \n4.  Сменить статус объявления \n5. Покупка недвжимости \n6. Вывод средств \n7. Доступная недвижимость \n8. Доступные объявления \n9. Баланс на смарт контракте \n10. Баланс на аккаунте \n11. Пополнить баланс смарт контракта \n12. Выход \n"))
             match choice:
                 case 1:
                     create_estate(account)
                 case 2:
                     create_ad(account)
                 case 3:
-                    pass
+                    change_status_estate(account)
                 case 4:
-                    withdraw(account)
+                    change_status_ad(account)
                 case 5:
-                    pass
+                    buy_estate(account)
                 case 6:
-                    pass
+                    withdraw(account)
                 case 7:
                     get_estate(account)
                 case 8:
@@ -139,7 +235,7 @@ def main():
                 case 10:
                     print(f"Баланс аккаунта: {w3.eth.get_balance(account)}")
                 case 11:
-                    pass
+                    pay(account)
                 case 12:
                     account = ""
                 case _:
